2dfdfefbd69a5e4f842ebcb5fbd25b024547a212
Pedro Nery <pedronas1201@gmail.com>
2024-11-09 19:39:14 +0000

    Enviando o estado do botão via bluetooth

--- main/main.c
+++ main/main.c
@@ -5,21 +5,28 @@
 #include "hardware/adc.h"
 #include "pico/stdlib.h"
 #include "server_common.h"
+#include "att_server.h"
 
 #define HEARTBEAT_PERIOD_MS 1000
+#define BUTTON_CHAR_HANDLE 0x0012
 
 static btstack_timer_source_t heartbeat;
 static btstack_packet_callback_registration_t hci_event_callback_registration;
+
+static uint8_t button_state = 0;
+const int BTN_PIN = 14;
 
 static void heartbeat_handler(struct btstack_timer_source *ts) {
     static uint32_t counter = 0;
     counter++;
 
-    // Update the temp every 10s
+    // Update a cada 10s da temp e do botão
     if (counter % 10 == 0) {
         poll_temp();
+        button_state = !gpio_get(BTN_PIN);
         if (le_notification_enabled) {
             att_server_request_can_send_now_event(con_handle);
+            att_server_notify(con_handle, BUTTON_CHAR_HANDLE, &button_state, sizeof(button_state));
         }
     }
 
@@ -35,6 +42,9 @@ static void heartbeat_handler(struct btstack_timer_source *ts) {
 
 int main() {
     stdio_init_all();
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
 
     // initialize CYW43 driver architecture (will enable BT if/because CYW43_ENABLE_BLUETOOTH == 1)
     if (cyw43_arch_init()) {
@@ -72,7 +82,6 @@ int main() {
 
     while(1) {
         tight_loop_contents();
-
     }
     return 0;
 }
