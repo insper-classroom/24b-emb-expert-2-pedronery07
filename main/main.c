0435278756683cf6320d97ecc2d75ba20ad061a6
Pedro Nery <pedronas1201@gmail.com>
2024-11-27 20:12:16 +0000

    correção variáveis globais

--- main/main.c
+++ main/main.c
@@ -41,41 +41,42 @@
 
 #define READ_BIT 0x80
 
-int32_t t_fine;
-
-uint16_t dig_T1;
-int16_t dig_T2, dig_T3;
-uint16_t dig_P1;
-int16_t dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
-uint8_t dig_H1, dig_H3;
-int8_t dig_H6;
-int16_t dig_H2, dig_H4, dig_H5;
+struct infos {
+    int32_t t_fine;
+    uint16_t dig_T1;
+    int16_t dig_T2, dig_T3;
+    uint16_t dig_P1;
+    int16_t dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
+    uint8_t dig_H1, dig_H3;
+    int8_t dig_H6;
+    int16_t dig_H2, dig_H4, dig_H5;
+};
 
 /* The following compensation functions are required to convert from the raw ADC
 data from the chip to something usable. Each chip has a different set of
 compensation parameters stored on the chip at point of manufacture, which are
 read from the chip at startup and used in these routines.
 */
-int32_t compensate_temp(int32_t adc_T) {
+int32_t compensate_temp(int32_t adc_T, struct infos inf) {
     int32_t var1, var2, T;
-    var1 = ((((adc_T >> 3) - ((int32_t) dig_T1 << 1))) * ((int32_t) dig_T2)) >> 11;
-    var2 = (((((adc_T >> 4) - ((int32_t) dig_T1)) * ((adc_T >> 4) - ((int32_t) dig_T1))) >> 12) * ((int32_t) dig_T3))
+    var1 = ((((adc_T >> 3) - ((int32_t) inf.dig_T1 << 1))) * ((int32_t) inf.dig_T2)) >> 11;
+    var2 = (((((adc_T >> 4) - ((int32_t) inf.dig_T1)) * ((adc_T >> 4) - ((int32_t) inf.dig_T1))) >> 12) * ((int32_t) inf.dig_T3))
             >> 14;
 
-    t_fine = var1 + var2;
-    T = (t_fine * 5 + 128) >> 8;
+    inf.t_fine = var1 + var2;
+    T = (inf.t_fine * 5 + 128) >> 8;
     return T;
 }
 
-uint32_t compensate_pressure(int32_t adc_P) {
+uint32_t compensate_pressure(int32_t adc_P, struct infos inf) {
     int32_t var1, var2;
     uint32_t p;
-    var1 = (((int32_t) t_fine) >> 1) - (int32_t) 64000;
-    var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * ((int32_t) dig_P6);
-    var2 = var2 + ((var1 * ((int32_t) dig_P5)) << 1);
-    var2 = (var2 >> 2) + (((int32_t) dig_P4) << 16);
-    var1 = (((dig_P3 * (((var1 >> 2) * (var1 >> 2)) >> 13)) >> 3) + ((((int32_t) dig_P2) * var1) >> 1)) >> 18;
-    var1 = ((((32768 + var1)) * ((int32_t) dig_P1)) >> 15);
+    var1 = (((int32_t) inf.t_fine) >> 1) - (int32_t) 64000;
+    var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * ((int32_t) inf.dig_P6);
+    var2 = var2 + ((var1 * ((int32_t) inf.dig_P5)) << 1);
+    var2 = (var2 >> 2) + (((int32_t) inf.dig_P4) << 16);
+    var1 = (((inf.dig_P3 * (((var1 >> 2) * (var1 >> 2)) >> 13)) >> 3) + ((((int32_t) inf.dig_P2) * var1) >> 1)) >> 18;
+    var1 = ((((32768 + var1)) * ((int32_t) inf.dig_P1)) >> 15);
     if (var1 == 0)
         return 0;
 
@@ -85,22 +86,22 @@ uint32_t compensate_pressure(int32_t adc_P) {
     else
         p = (p / (uint32_t) var1) * 2;
 
-    var1 = (((int32_t) dig_P9) * ((int32_t) (((p >> 3) * (p >> 3)) >> 13))) >> 12;
-    var2 = (((int32_t) (p >> 2)) * ((int32_t) dig_P8)) >> 13;
-    p = (uint32_t) ((int32_t) p + ((var1 + var2 + dig_P7) >> 4));
+    var1 = (((int32_t) inf.dig_P9) * ((int32_t) (((p >> 3) * (p >> 3)) >> 13))) >> 12;
+    var2 = (((int32_t) (p >> 2)) * ((int32_t) inf.dig_P8)) >> 13;
+    p = (uint32_t) ((int32_t) p + ((var1 + var2 + inf.dig_P7) >> 4));
 
     return p;
 }
 
-uint32_t compensate_humidity(int32_t adc_H) {
+uint32_t compensate_humidity(int32_t adc_H, struct infos inf) {
     int32_t v_x1_u32r;
-    v_x1_u32r = (t_fine - ((int32_t) 76800));
-    v_x1_u32r = (((((adc_H << 14) - (((int32_t) dig_H4) << 20) - (((int32_t) dig_H5) * v_x1_u32r)) +
-                   ((int32_t) 16384)) >> 15) * (((((((v_x1_u32r * ((int32_t) dig_H6)) >> 10) * (((v_x1_u32r *
-                                                                                                  ((int32_t) dig_H3))
+    v_x1_u32r = (inf.t_fine - ((int32_t) 76800));
+    v_x1_u32r = (((((adc_H << 14) - (((int32_t) inf.dig_H4) << 20) - (((int32_t) inf.dig_H5) * v_x1_u32r)) +
+                   ((int32_t) 16384)) >> 15) * (((((((v_x1_u32r * ((int32_t) inf.dig_H6)) >> 10) * (((v_x1_u32r *
+                                                                                                  ((int32_t) inf.dig_H3))
             >> 11) + ((int32_t) 32768))) >> 10) + ((int32_t) 2097152)) *
-                                                 ((int32_t) dig_H2) + 8192) >> 14));
-    v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) * ((int32_t) dig_H1)) >> 4));
+                                                 ((int32_t) inf.dig_H2) + 8192) >> 14));
+    v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) * ((int32_t) inf.dig_H1)) >> 4));
     v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
     v_x1_u32r = (v_x1_u32r > 419430400 ? 419430400 : v_x1_u32r);
 
@@ -146,34 +147,34 @@ static void read_registers(uint8_t reg, uint8_t *buf, uint16_t len) {
 }
 
 /* This function reads the manufacturing assigned compensation parameters from the device */
-void read_compensation_parameters() {
+void read_compensation_parameters(struct infos inf) {
     uint8_t buffer[26];
 
     read_registers(0x88, buffer, 26);
 
-    dig_T1 = buffer[0] | (buffer[1] << 8);
-    dig_T2 = buffer[2] | (buffer[3] << 8);
-    dig_T3 = buffer[4] | (buffer[5] << 8);
+    inf.dig_T1 = buffer[0] | (buffer[1] << 8);
+    inf.dig_T2 = buffer[2] | (buffer[3] << 8);
+    inf.dig_T3 = buffer[4] | (buffer[5] << 8);
 
-    dig_P1 = buffer[6] | (buffer[7] << 8);
-    dig_P2 = buffer[8] | (buffer[9] << 8);
-    dig_P3 = buffer[10] | (buffer[11] << 8);
-    dig_P4 = buffer[12] | (buffer[13] << 8);
-    dig_P5 = buffer[14] | (buffer[15] << 8);
-    dig_P6 = buffer[16] | (buffer[17] << 8);
-    dig_P7 = buffer[18] | (buffer[19] << 8);
-    dig_P8 = buffer[20] | (buffer[21] << 8);
-    dig_P9 = buffer[22] | (buffer[23] << 8);
+    inf.dig_P1 = buffer[6] | (buffer[7] << 8);
+    inf.dig_P2 = buffer[8] | (buffer[9] << 8);
+    inf.dig_P3 = buffer[10] | (buffer[11] << 8);
+    inf.dig_P4 = buffer[12] | (buffer[13] << 8);
+    inf.dig_P5 = buffer[14] | (buffer[15] << 8);
+    inf.dig_P6 = buffer[16] | (buffer[17] << 8);
+    inf.dig_P7 = buffer[18] | (buffer[19] << 8);
+    inf.dig_P8 = buffer[20] | (buffer[21] << 8);
+    inf.dig_P9 = buffer[22] | (buffer[23] << 8);
 
-    dig_H1 = buffer[25]; // 0xA1
+    inf.dig_H1 = buffer[25]; // 0xA1
 
     read_registers(0xE1, buffer, 8);
 
-    dig_H2 = buffer[0] | (buffer[1] << 8); // 0xE1 | 0xE2
-    dig_H3 = (int8_t) buffer[2]; // 0xE3
-    dig_H4 = buffer[3] << 4 | (buffer[4] & 0xf); // 0xE4 | 0xE5[3:0]
-    dig_H5 = (buffer[4] >> 4) | (buffer[5] << 4); // 0xE5[7:4] | 0xE6
-    dig_H6 = (int8_t) buffer[6]; // 0xE7
+    inf.dig_H2 = buffer[0] | (buffer[1] << 8); // 0xE1 | 0xE2
+    inf.dig_H3 = (int8_t) buffer[2]; // 0xE3
+    inf.dig_H4 = buffer[3] << 4 | (buffer[4] & 0xf); // 0xE4 | 0xE5[3:0]
+    inf.dig_H5 = (buffer[4] >> 4) | (buffer[5] << 4); // 0xE5[7:4] | 0xE6
+    inf.dig_H6 = (int8_t) buffer[6]; // 0xE7
 }
 
 static void bme280_read_raw(int32_t *humidity, int32_t *pressure, int32_t *temperature) {
@@ -197,6 +198,7 @@ int display() {
 int main() {
     stdio_init_all();
     printf("Hello, bme280! Reading raw data from registers via SPI...\n");
+    struct infos informacoes;
 
     // This example will use SPI0 at 0.5MHz.
     spi_init(spi_default, 500 * 1000);
@@ -218,7 +220,7 @@ int main() {
     read_registers(0xD0, &id, 1);
     printf("Chip ID is 0x%x\n", id);
 
-    read_compensation_parameters();
+    read_compensation_parameters(informacoes);
 
     write_register(0xF2, 0x1); // Humidity oversampling register - going for x1
     write_register(0xF4, 0x27);// Set rest of oversampling modes and run mode to normal
@@ -231,9 +233,9 @@ int main() {
 
         // These are the raw numbers from the chip, so we need to run through the
         // compensations to get human understandable numbers
-        temperature = compensate_temp(temperature);
-        pressure = compensate_pressure(pressure);
-        humidity = compensate_humidity(humidity);
+        temperature = compensate_temp(temperature, informacoes);
+        pressure = compensate_pressure(pressure, informacoes);
+        humidity = compensate_humidity(humidity, informacoes);
 
         printf("Humidity = %.2f%%\n", humidity / 1024.0);
         printf("Pressure = %dPa\n", pressure);
